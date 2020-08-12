13648fcf265934f17d131539ddb8b2cbf73cb80c
Aux-084 <dinothan1@gmail.com>
2020-08-12 13:40:17 +0530

    SignIn component Validation

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -13,7 +13,27 @@ import {
 class SignIn extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: '',
+      validEmail: true,
+      validPassword: true,
+    };
   }
+  onClickButton = () => {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (reg.test(this.state.email) === false) {
+      this.setState({validEmail: false});
+    } else {
+      this.setState({validEmail: true});
+    }
+
+    if (this.state.password === '') {
+      this.setState({validPassword: false});
+    } else {
+      this.setState({validPassword: true});
+    }
+  };
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -24,29 +44,41 @@ class SignIn extends Component {
           <View style={{flexDirection: 'row', marginBottom: 18}}>
             <Text style={{marginRight: 52}}>Email</Text>
             <TextInput
-              style={{
-                height: 40,
-                borderColor: 'black',
-                borderWidth: 1,
-                width: 200,
-              }}
-              //   onChangeText={(text) => this.setState({text})}
-              //   value={this.state.text}
+              style={[
+                this.state.validEmail
+                  ? {
+                      borderColor: 'black',
+                    }
+                  : {borderColor: 'red'},
+                {
+                  height: 40,
+                  borderWidth: 1,
+                  width: 200,
+                },
+              ]}
+              onChangeText={(text) => this.setState({email: text})}
+              value={this.state.email}
             />
           </View>
           <View style={{flexDirection: 'row'}}>
             <Text style={{marginRight: 25}}>Password</Text>
             <TextInput
-              style={{
-                height: 40,
-                borderColor: 'black',
-                borderWidth: 1,
-                width: 200,
-              }}
+              style={[
+                this.state.validPassword
+                  ? {
+                      borderColor: 'black',
+                    }
+                  : {borderColor: 'red'},
+                {borderWidth: 1, width: 200, height: 40},
+              ]}
+              onChangeText={(text) => this.setState({password: text})}
+              value={this.state.password}
             />
           </View>
 
-          <TouchableOpacity style={{marginLeft: 150}}>
+          <TouchableOpacity
+            style={{marginLeft: 150}}
+            onPress={this.onClickButton}>
             <View
               style={{
                 backgroundColor: '#D8D4D3',
